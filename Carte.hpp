9381a680aebe7d32eaddf7470d0523515b320690
Tongman <quentin.lecorvec@gmail.com>
2014-09-22 13:00:35 +0200

    Create Carte.hpp

---
+++ Carte.hpp
@@ -0,0 +1,40 @@
+/*
+  Fichier Carte.hpp
+  Définition de la classe Carte
+  Auteur : LE CORVEC Quentin Cdrik Boua
+*/
+
+/****************************************************************************************************/
+
+#ifndef CARTE_HPP
+#define CARTE_HPP
+
+/****************************************************************************************************/
+
+class Carte {
+  private :
+    //Attributs
+    protected String nom;
+    protected String description;
+
+  public :
+    //Constructeur
+    Carte(String n); //Constructeur par défaut
+    Carte(String n, String d);
+
+    //Destructeur
+    ~Carte();
+
+    //Accesseurs
+    void afficher() const;
+
+    //Mutateurs
+
+}; //Class Carte
+
+/****************************************************************************************************/
+
+#include "Carte.cpp" //Implémentation de Carte
+#endif // CARTE_HPP
+
+/****************************************************************************************************/
