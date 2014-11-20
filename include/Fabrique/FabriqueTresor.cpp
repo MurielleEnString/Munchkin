4d800f7f48947bce420e38d4066d910085a7c48f
MurielleEnString <MurielleEnString@users.noreply.github.com>
2014-11-20 13:08:23 +0100

    Update FabriqueTresor.cpp

--- include/Fabrique/FabriqueTresor.cpp
+++ include/Fabrique/FabriqueTresor.cpp
@@ -1,26 +1,28 @@
 #include "FabriqueTresor.hpp"
 
 
-FabriqueTresor::FabriqueTresor(){}
+FabriqueTresor::FabriqueTresor():FabriqueCarte(){
+	fe=new FabriqueEffet();
+}
+
 Carte * FabriqueTresor::fabriquerCarte(vector<string> champs){
 	
+	Effet * e=fe->fabriquerEffet(champs);
 	if(champs.front()=="Potion"){
-		if(champs.size()>5){
-			Effet * e=fe->fabriquerEffet(champs);
-		}
-		return new Potion(champs[1],champs[2],NULL,stoi(champs[3]),stoi(champs[4]));
+		return new Potion(champs[1],champs[2],e,stoi(champs[3]),stoi(champs[4]));
 	}
 	else if(champs.front()=="Equipement"){
-		return new Equipement(champs[1],champs[2],stoi(champs[3]),stoi(champs[4]),NULL);
+		return new Equipement(champs[1],champs[2],stoi(champs[3]),stoi(champs[4]),e);
 	}
 	else if(champs.front()=="Armure"){
-	
+		return new Armure(champs[1],champs[2],stoi(champs[3]),stoi(champs[4]),e);
 	}
 	else if(champs.front()=="Main"){
-	
+		if(champs.back()=="2") return new Main(champs[1],champs[2],stoi(champs[3]),stoi(champs[4]),e,stoi(champs.back()));
+		else return new Main(champs[1],champs[2],stoi(champs[3]),stoi(champs[4]),e);
 	}
 	else if(champs.front()=="Chaussure"){
-	
+		return new Chaussure(champs[1],champs[2],stoi(champs[3]),stoi(champs[4]),e);
 	}
 	else if(champs.front()=="CouvreChef"){
 	
